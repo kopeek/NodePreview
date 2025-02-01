42ab233a16d0bcef28b820130df076b37c307a8e
kopeek <111kooprtsu@gmail.com>
2025-02-01 20:28:56 +0100

    added icons etc.

--- node_2d.gd
+++ node_2d.gd
@@ -5,4 +5,4 @@ func _ready() -> void:
 	var image: Image = await $NodePreview.get_image()
 	var texture: = ImageTexture.create_from_image(image)
 	
-	#$TextureRect.texture = texture
+	$TextureRect.texture = texture
